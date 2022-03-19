920a7d7dd49e6c97c53b039f41a5a49c0817e6ea
lucciii33 <angelomaiele@gmail.com>
2022-03-19 21:50:35 +0000

    try

--- src/js/views/home.js
+++ src/js/views/home.js
@@ -1,15 +1,94 @@
-import React from "react";
+import React,{useState} from "react";
 import rigoImage from "../../img/rigo-baby.jpg";
 import "../../styles/home.css";
 
-export const Home = () => (
-	<div className="text-center mt-5">
-		<h1>Hello Rigo!</h1>
-		<p>
-			<img src={rigoImage} />
-		</p>
-		<a href="#" className="btn btn-success">
-			If you see this green button, bootstrap is working
-		</a>
-	</div>
-);
+export const Home = () => {
+	const [color, setColor] = useState("grey");
+	const [color1, setColor1] = useState("grey");
+	const [color2, setColor2] = useState("grey");
+	const [color3, setColor3] = useState("grey");
+	var timer = null;
+	function switchColor() {
+		if (color == "grey") setColor("red");
+	}
+	function switchClockStatus() {
+		if (timer == null) {
+			timer = setInterval(switchColor, 3000);
+			console.log(timer);
+		} else {
+			clearInterval(timer);
+			timer = null;
+			console.log(timer);
+		}
+	}
+
+	switchClockStatus();
+
+	return (
+		<div className="text-center mt-5 position-relative">
+			<div className=" position-absolute top-50 start-50">
+				<div>
+					<div className="semaforo m-2 mb-0">semaforo</div>
+
+
+				<div className="d-flex mb-0">
+					<div className="sema m-2 mt-0">
+						<div className={color == "grey" ? "red" : "grey"}
+					onClick={() => setColor("grey")}></div>
+					</div>
+					<div className="sema m-2 mt-0">
+						<div className={color1 == "grey" ? "red" : "grey"}
+					onClick={() => setColor("grey")}></div>
+					</div>
+					<div className="sema m-2 mt-0">
+						<div className={color2 == "grey" ? "red" : "grey"}
+					onClick={() => setColor("grey")}></div>
+					</div>
+					<div className="sema m-2 mt-0">
+						<div className={color3 == "grey" ? "red" : "grey"}
+					onClick={() => setColor("grey")}></div>
+					</div>
+					<div className="sema m-2 mt-0">
+						<div className={color == "grey" ? "red" : "grey"}
+					onClick={() => setColor("grey")}></div>
+					</div>
+					<div className="sema m-2 mt-0">
+						<div className={color == "grey" ? "red" : "grey"}
+					onClick={() => setColor("grey")}></div>
+					</div>
+				</div>
+
+				<div className="d-flex mt-0">
+					<div className="sema m-2 mt-0">
+						<div className={color == "grey" ? "red" : "grey"}
+					onClick={() => setColor("grey")}></div>
+					</div>
+					<div className="sema m-2 mt-0">
+						<div className={color == "grey" ? "red" : "grey"}
+					onClick={() => setColor("grey")}></div>
+					</div>
+					<div className="sema m-2 mt-0">
+						<div className={color == "grey" ? "red" : "grey"}
+					onClick={() => setColor("grey")}></div>
+					</div>
+					<div className="sema m-2 mt-0">
+						<div className={color == "grey" ? "red" : "grey"}
+					onClick={() => setColor("grey")}></div>
+					</div>
+					<div className="sema m-2 mt-0">
+						<div className={color == "grey" ? "red" : "grey"}
+					onClick={() => setColor("grey")}></div>
+					</div>
+					<div className="sema m-2 mt-0">
+						<div className={color == "grey" ? "red" : "grey"}
+					onClick={() => setColor("grey")}></div>
+					</div>
+				</div>
+				</div>
+				
+			</div>
+
+
+		</div>
+	)
+} 
